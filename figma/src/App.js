739d081a7672f9f47da1d1fafc31be2b043ada86
pravee-develop <praveensj74@gmail.com>
2024-08-06 15:05:44 +0530

    first comit

---
+++ figma/src/App.js
@@ -0,0 +1,263 @@
+import "./App.css"
+
+
+function App(){
+    return(
+        <div className="document">
+            {/* left section */}
+            <div className="leftside">
+               <div className="logo">
+               <h2><i class='bx bxl-gitlab'></i> Z<span >euron.ai</span></h2>
+               </div>
+
+               <div className="mic">
+                <div className="mic1">
+                <i class='bx bxs-microphone' ></i> 
+                </div>
+                <div className="mic2">
+                Emergency <br></br>icon
+                </div>
+               
+               </div>
+
+               <div className="nav-bar">
+                
+                        <h4> <i class='bx bx-grid' ></i> Example 1</h4>
+                        <h4><i class='bx bx-credit-card-alt'></i> Onboard Patient</h4>
+                        <h4> <i class='bx bxs-time' ></i> Example 1</h4>
+                        <h4> <i class='bx bx-notepad'></i> Example 1</h4>
+                        <h4><i class='bx bxs-time' ></i>  Example 1</h4>
+                        <h4> <i class='bx bxl-microsoft-teams'></i>Example 1</h4>
+                        <h4><i class='bx bxl-yahoo'></i> Example 1</h4>
+                        <h4> <i class='bx bxs-notification'></i> Example 1</h4>
+                        <h4> <i class='bx bxl-telegram' ></i> Example 1</h4>
+                 
+               </div>
+               <div className="lastbar">
+               <h4><i class='bx bx-cog' ></i> Settings</h4>
+               </div>
+                
+
+            </div>
+
+
+            {/* left side  end*/}
+
+
+
+            {/* middlesection start */}
+
+           <div className="main">
+                <h1>Assignment</h1>
+           <div className="middlesection">
+                <div className="toppart">
+                    <div className="box-ctn">
+                        <div className="box"></div>
+                        <div className="ctn">
+                            <h2>
+                                Jerome Bellingham <button>MEMEBER</button>
+                            </h2>
+                            <h3><i class='bx bx-anchor' ></i> Joined Since : 12 March 2023</h3>
+                        </div>
+
+                    </div>
+                    <br></br>
+
+                    <div className="socialmedia">
+
+                             <i class='bx bx-envelope' ></i>
+                             <i class='bx bx-phone' ></i>
+                             <i class='bx bx-mobile-alt'></i>
+                             <i class='bx bx-video' ></i>
+                             <i class='bx bx-edit' ></i>
+                             <br></br>
+                             <input type="radio" name="rad"></input>
+                             <input type="radio" name="rad"></input>
+                             <input type="radio" name="rad"></input>
+                             <input type="radio" name="rad"></input>
+                             <input type="radio" name="rad"></input>
+                    </div>
+                </div>
+                {/* toppart end */}
+
+                <div className="middlepart">
+                    <h1>Basic Information</h1>
+
+                    <br></br>
+                    <h3><i class='bx bx-female-sign' ></i>Gender</h3>
+                    <p>male</p>
+                    <br></br>
+
+                    <h3><i class='bx bx-cake' ></i>  Birthday</h3>
+                    <p>12 August 2001</p>
+                    <br></br>
+
+                    <h3> <i class='bx bx-phone' ></i> Phone Number</h3>
+                    <p> +62 837 356 343 23</p>
+                    <br></br>
+
+                    <h3> <i class='bx bx-envelope' ></i>  Email</h3>
+                    <p>jeromebellingham93@gmail.com</p>
+                    <br></br>
+
+                    <h3> <i class='bx bxs-radiation'></i> Source</h3>
+                    <p>
+                        <i class='bx bxl-messenger'></i> <i class='bx bxl-google' ></i> <i class='bx bxl-telegram' ></i> <i class='bx bxl-whatsapp' ></i> 
+                        
+                    </p>
+
+                </div>
+
+                {/* middle part end */}
+
+                <div className="middlelast">
+                    <div className="lasthead">
+                        <h3>Data of assurance</h3>
+                        <i class='bx bx-dots-horizontal-rounded' ></i>
+                    </div>
+                    <div className="lastmiddle">
+                        <img src="https://tse3.mm.bing.net/th?id=OIP.GcTMf_3Vlc1daLMmRLNJ_QAAAA&pid=Api&P=0&h=180" width={"50px"}  height={"50px"}></img>
+                        <p>234-234-232-32</p>
+                    </div>
+                    <div className="lastbtm">
+                        <div>
+                        <h2>Expiry</h2>
+                        <p>23/04/2024</p>
+                        </div>
+                        <div>
+                        <h2>Status</h2>
+                        <p>Actived</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+           </div>
+                {/* middlesection end */}
+
+
+
+                <div className="leftsection">
+                <div className="lefttopbtn">
+
+                   <div className="lefttop">
+                        <div className="leftbtn">Assessment</div>
+                        <div className="rightbtn">Training</div>
+                    </div>
+                    {/* lefttop end */}
+                    <div className="leftcontent">
+                       <div className="contents">
+
+                       <div className="cont">
+                            <p><i class='bx bxs-folder-minus' ></i>
+                            </p>
+                            <h2>Standards</h2>
+                        </div>
+
+                        <div className="cont"> 
+                            <h3><i class='bx bx-edit' ></i></h3>
+                            <h4><i class='bx bx-trash' ></i></h4>
+                        </div>
+                       </div>
+
+                       <hr></hr>
+
+                        <div className="contents">
+
+                       <div className="cont">
+                            <p><i class='bx bx-folder' ></i>
+                            </p>
+                            <h2>xx protocol</h2>
+                        </div>
+
+                        <div className="cont">
+                            <h3><i class='bx bx-edit' ></i></h3>
+                            <h4><i class='bx bx-trash' ></i></h4>
+                        </div>
+                       </div>
+
+                       <hr></hr>
+
+                       <div className="contents">
+
+                       <div className="cont">
+                            <p><i class='bx bx-folder' ></i>
+                            </p>
+                            <h2>Create New</h2>
+                        </div>
+
+                        <div className="cont">
+                            <h3><i class='bx bx-edit' ></i></h3>
+                            <h4><i class='bx bx-trash' ></i></h4>
+                        </div>
+                       </div>
+
+
+                    </div>
+
+                </div>
+                    {/* left content ending */}
+
+                    <div className="leftpart">
+
+                        <div className="leftsidebtm1">
+
+                            <div className="hello">
+                                <h2>Case History Files</h2>
+                               <h2> <i class='bx bx-dots-horizontal-rounded' ></i></h2>
+                            </div>
+
+                            {/* leftside contents */}
+                            {/* 1 */}
+
+                            <div className="filecontent">
+                            <i class='bx bx-file' ></i>
+                            <h4>Agreement_Menditation.zip <br></br>2.3 mb</h4>
+                            <i class='bx bxs-cloud-download' ></i>
+                            </div>
+                            {/* 2 */}
+
+                            <div className="filecontent">
+                            <i class='bx bx-file' ></i>
+                            <h4>Provision of information.zip <br></br>2.3 mb</h4>
+                            <i class='bx bxs-cloud-download' ></i>
+
+                            </div>
+
+                            {/* 3 */}
+                            <div className="filecontent">
+                            <i class='bx bx-file' ></i>
+                            <h4>Agreement_Menditation.zip <br></br>2.3 mb</h4>
+                            <i class='bx bxs-cloud-download' ></i>
+
+                            </div>
+
+                            {/* 4 */}
+                            <div className="filecontent">
+                            <i class='bx bx-file' ></i>
+                            <h4>Provision of information.zip  <br></br>2.3 mb</h4>
+                            <i class='bx bxs-cloud-download' ></i>
+
+                            </div>
+                            </div>
+
+
+                        <div className="leftsidebtm2">
+                            <h2>Appointement Schedule</h2>
+                            <h3><i class='bx bx-plus' ></i></h3>
+                        </div>
+                        </div>
+
+
+                   
+                    
+                        {/* left side btm 1 */}
+                       
+                    
+                </div>
+        </div>
+    )
+}
+
+export default App
